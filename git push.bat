cd0b89b8d490c8625763c284c45ea0c07b5d8684
榭壹Shey <z_shey@163.com>
2023-12-12 09:34:48 +0800

    Commit message

---
+++ git push.bat
@@ -0,0 +1,6 @@
+@echo off
+
+git add .
+git commit -m "Commit message"
+git push origin main
+pause
--- java-practice/java-school-curriculum-experiment/Experiment_08/src/JtdsExample.java
+++ java-practice/java-school-curriculum-experiment/Experiment_08/src/JtdsExample.java
@@ -6,35 +6,20 @@ import java.sql.Statement;
 public class JtdsExample {
 
     public static void main(String[] args) {
-        try {
-            // 加载jTDS驱动程序
-            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+        String URL = "jdbc:jtds:sqlserver://192.168.136.144/Algorithm_SQL";
+        String USERNAME = "sa";
+        String PASSWORD = "Root123";
 
-            // 建立与SQL Server数据库的连接
-            String url = "jdbc:jtds:sqlserver://192.168.136.141/Algorithm_SQL";
-            String username = "sa";
-            String password = "Root123";
-
-            Connection connection = DriverManager.getConnection(url, username, password);
-
-            // 执行查询
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM ALG.Alumni");
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM ALG.Alumni")) {
 
             while (resultSet.next()) {
-                // 处理查询结果
                 int id = resultSet.getInt("sno");
                 String name = resultSet.getString("email");
-                // 其他列类似地获取数据
-
                 System.out.println("ID: " + id + ", Name: " + name);
             }
 
-            resultSet.close();
-            statement.close();
-
-            // 关闭连接
-            connection.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
